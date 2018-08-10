bf66acfa91c24f1699c434fbf8286d60a4fb7053
Counttalkula666 <elenabelgum@gmail.com>
2018-08-09 23:27:48 -0700

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,11 @@
-//Psuedocode
+//Psuedocode 
+//start button
+//form w/ questions and choices - only one choice allowed
+//Find the selected answer in the HTML.
+//Handle what happens if the answer is correct.
+//Handle what happens if the answer is wrong.
+//timer
+//done button
+//answer/score calculator at end
+
+
